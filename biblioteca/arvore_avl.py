23710d476f7cc8828e0a87c1b6451ea28eb72a39
RhannaKaroline <rhannakaroline02@gmail.com>
2025-11-18 21:09:34 -0400

    arvore_avl.py

--- biblioteca/arvore_avl.py
+++ biblioteca/arvore_avl.py
@@ -1,7 +1,6 @@
 from biblioteca.livro import Livro
 from biblioteca.node import Node
 
-
 class ArvoreAVL:
     def __init__(self):
         self.raiz = None
@@ -150,3 +149,50 @@ class ArvoreAVL:
         while atual.esquerda is not None:
             atual = atual.esquerda
         return atual
+    
+    #Rhanna
+    #Métodos de impressão
+
+    def imprimir_in_order(self):
+        print("\n--- Impressão In-Order (ordenada por ID) ---")
+        self._in_order(self.raiz)
+
+    def _in_order(self, node):
+        if node:
+            self._in_order(node.esquerda)
+            print(f"ID: {node.livro.id} | Título: {node.livro.titulo}")
+            self._in_order(node.direita)
+
+    def imprimir_pre_order(self):
+        print("\n--- Impressão Pré-Order ---")
+        self._pre_order(self.raiz)
+
+    def _pre_order(self, node):
+        if node:
+            print(f"ID: {node.livro.id} | Título: {node.livro.titulo}")
+            self._pre_order(node.esquerda)
+            self._pre_order(node.direita)
+
+    def imprimir_pos_order(self):
+        print("\n--- Impressão Pós-Order ---")
+        self._pos_order(self.raiz)
+
+    def _pos_order(self, node):
+        if node:
+            self._pos_order(node.esquerda)
+            self._pos_order(node.direita)
+            print(f"ID: {node.livro.id} | Título: {node.livro.titulo}")
+
+    def imprimir_livros_por_id(self):
+        print("\n--- Livros ordenados pelo ID ---")
+        self.imprimir_in_order()
+
+    def visualizar(self):
+        print("\n--- Visualização da Árvore AVL ---")
+        self._print_tree(self.raiz, 0)
+
+    def _print_tree(self, node, nivel):
+        if node is not None:
+            self._print_tree(node.direita, nivel + 1)
+            print("    " * nivel + f"→ (ID {node.livro.id})")
+            self._print_tree(node.esquerda, nivel + 1)
